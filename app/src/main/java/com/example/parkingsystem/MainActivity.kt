5f55a43c57bd464f0c11e5092721021d2f89c660
mimirandaipvc <mimiranda@ipvc.pt>
2022-05-20 23:33:49 +0100

    Ver, Navegar, detalhes de mapas

--- app/src/main/java/com/example/parkingsystem/MainActivity.kt
+++ app/src/main/java/com/example/parkingsystem/MainActivity.kt
@@ -1,12 +1,277 @@
 package com.example.parkingsystem
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.*
+import android.location.Location
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnInfoWindowClickListener {
+
+    private var mMap: GoogleMap? = null
+    internal var mGoogleApiClient: GoogleApiClient? = null
+    internal lateinit var mLocationRequest: LocationRequest
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 111
+    private lateinit var locationAtual : Location
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //teste
+
+        getLocation();
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.myMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val btnsearch = findViewById<Button>(R.id.proc)
+        btnsearch.setOnClickListener {
+            val locationSearch: EditText = findViewById(R.id.et_search)
+            var location: String
+            location = locationSearch.text.toString().trim()
+            var addressList: List<Address>? = null
+
+            if (location == null || location == ""){
+                Toast.makeText(this, "provide location", Toast.LENGTH_SHORT).show()
+            }else{
+                val geoCoder = Geocoder(this)
+                try {
+                    addressList = geoCoder.getFromLocationName(location, 1)
+                }catch (e: IOException){
+                    e.printStackTrace()
+                }
+
+                val address = addressList!![0]
+                val latLng = LatLng(address.latitude, address.longitude)
+                mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+            }
+        }
     }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if(ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                locationPermissionCode
+            )
+        }else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+        }
+    }
+
+
+    fun getDirectionURL(origin:LatLng, dest:LatLng) : String{
+       return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&sensor=false&mode=driving&key=AIzaSyBmtZXVwPpAqRuMkOOfQlsrCUD-RsIBT_0"
+    }
+
+    inner class GetDirection(val url : String) : AsyncTask<Void, Void, List<List<LatLng>>>() {
+        override fun doInBackground(vararg p0: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body!!.string()
+            val result = ArrayList<List<LatLng>>()
+            try {
+                val respObj = Gson().fromJson(data, GoogleMapDTO::class.java)
+
+                val path = ArrayList<LatLng>()
+
+                for(i in 0..(respObj.routes[0].legs[0].steps.size-1)){
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            } catch (e:Exception) {
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for(i in result.indices) {
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.BLUE)
+                lineoption.geodesic(true)
+            }
+            mMap?.addPolyline(lineoption)
+        }
+    }
+
+    public fun decodePolyline(encoded: String): List<LatLng> {
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while(b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while(b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            val latLng = LatLng((lat.toDouble() / 1E5), (lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+        return poly
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap;
+
+        //Adicionar Parques
+        val location = LatLng(41.691, -8.831)
+        mMap!!.addMarker(MarkerOptions().position(location).title("Teste0").snippet("Maisdfsdsds \n fsdds" ))
+        val location1 = LatLng(41.692, -8.832)
+        mMap!!.addMarker(MarkerOptions().position(location1).title("Teste1"))
+
+        mMap!!.setOnInfoWindowClickListener(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ){
+                buildGoogleApiClient()
+                mMap!!.isMyLocationEnabled = true
+            }
+        }else{
+            buildGoogleApiClient()
+            mMap!!.isMyLocationEnabled = true
+        }
+    }
+
+    protected fun buildGoogleApiClient(){
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API).build()
+        mGoogleApiClient!!.connect()
+    }
+
+    override fun onLocationChanged(location: Location) {
+        locationAtual = location
+        moveCamera(LatLng(location.latitude,location.longitude), 15f, "My Location")
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        this.mMap!!.isMyLocationEnabled = true
+
+    }
+    private fun moveCamera(latlng : LatLng, zoom : Float, title : String){
+        CoroutineScope(Main).launch {
+            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom))
+            if (!title.equals("My Location")) mMap!!.addMarker(MarkerOptions().position(latlng).title(title))
+        }
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 1000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ){
+            LocationServices.getFusedLocationProviderClient(this)
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
+    }
+
+    override fun onInfoWindowClick(p0: Marker) {
+        val lat = p0.position.latitude
+        val lng = p0.position.longitude
+        val rl : RelativeLayout = findViewById(R.id.rl)
+        rl.visibility = View.VISIBLE
+        val tv1 : TextView = findViewById(R.id.tv1)
+        tv1.setText("Nome: Parque da Feira")
+        val tv2 : TextView = findViewById(R.id.tv2)
+        tv2.setText("Morada: Rua 20 de Janeiro - Viana do Castelo. Lat: " + lat + " Long: " + lng)
+        val btn_rota = findViewById<Button>(R.id.btnrota)
+        btn_rota.setOnClickListener {
+            val location1 = LatLng(lat, lng)
+            //val location = LatLng(41.55, -8.423)
+            mMap!!.addMarker(MarkerOptions().position(LatLng(locationAtual.latitude, locationAtual.longitude)).title("XXXX").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)))
+            mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(locationAtual.latitude, locationAtual.longitude), 15f))
+            val URL = getDirectionURL(LatLng(locationAtual.latitude, locationAtual.longitude), location1)
+            GetDirection(URL).execute()
+        }
+        val btn_exit = findViewById<Button>(R.id.btnexit)
+        btn_exit.setOnClickListener {
+            rl.visibility = View.INVISIBLE
+        }
+    }
+
+    fun reservar(view: View) {}
 }
