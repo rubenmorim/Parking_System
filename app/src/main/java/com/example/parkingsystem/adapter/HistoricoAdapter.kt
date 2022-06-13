d30ba8042916d7f84a2c46d35e00f0ff8b72a68b
Ruben Morim <rubenmorim17@gmail.com>
2022-06-14 00:35:25 +0100

    Done historico

--- app/src/main/java/com/example/parkingsystem/adapter/HistoricoAdapter.kt
+++ app/src/main/java/com/example/parkingsystem/adapter/HistoricoAdapter.kt
@@ -1,13 +1,17 @@
 package com.example.parkingsystem.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parkingsystem.R
 import com.example.parkingsystem.model.Historico
 import kotlinx.android.synthetic.main.recyclerline.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class HistoricoAdapter(val historico: List<Historico>): RecyclerView.Adapter<HistoricoViewHolder>() {
@@ -31,11 +35,16 @@ class HistoricoViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
     private val name: TextView = itemView.findViewById(R.id.tvNome)
     private val dataEntrada:TextView = itemView.findViewById(R.id.tvDataEntrada)
     private val dataSaida:TextView = itemView.findViewById(R.id.tvDataSaida)
+    private val priceText:TextView = itemView.findViewById(R.id.priceText)
+    private val timeText:TextView = itemView.findViewById(R.id.timeText)
+
 
     fun bind(historico: Historico) {
         name.text = historico.nomeParque
         dataEntrada.text = historico.entrada
         dataSaida.text = historico.saida
+        priceText.text = historico.preco.toString() + "€"
+        timeText.text =  historico.tempo.toString() + "m"
     }
 
 }
--- app/src/main/java/com/example/parkingsystem/model/Historico.kt
+++ app/src/main/java/com/example/parkingsystem/model/Historico.kt
@@ -4,5 +4,7 @@ data class Historico (
     val idParque: Long,
     val nomeParque: String,
     val entrada: String,
-    val saida: String
+    val saida: String,
+    val tempo:Int,
+    val preco:Int,
 )
