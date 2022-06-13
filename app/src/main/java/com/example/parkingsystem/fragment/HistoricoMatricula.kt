6b29be106b32fd08e8d1b262b11bd4a648f8bede
Simao Ferreira <simaoferreira@ipvc.pt>
2022-06-13 11:05:12 +0100

    Botão AddVeiculos
    Botão AddVeiculos no histório Veiculos

--- app/src/main/java/com/example/parkingsystem/fragment/HistoricoMatricula.kt
+++ app/src/main/java/com/example/parkingsystem/fragment/HistoricoMatricula.kt
@@ -1,12 +1,15 @@
 package com.example.parkingsystem.fragment
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.icu.util.UniversalTimeScale.toLong
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parkingsystem.R
@@ -16,15 +19,20 @@ import com.example.parkingsystem.api.matricula.MatriculaEndpoint
 import com.example.parkingsystem.model.Matricula
 import com.example.parkingsystem.room.entity.User
 import kotlinx.android.synthetic.main.recyclerlinematricula.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import com.example.parkingsystem.MainActivity
+import com.example.parkingsystem.global.Global
 
 class HistoricoMatricula(user: User) : Fragment() {
 
     var m_Text: String =                                ""
+    var m_Text2: String =                               ""
     private var utilizador: User = user
-    private lateinit var historicoMatricula: HistoricoMatricula
 
     private fun setFragment(fragment: Fragment) {
         val fragmentTransaction = parentFragmentManager.beginTransaction()
@@ -63,7 +71,72 @@ class HistoricoMatricula(user: User) : Fragment() {
             }
         })
 
+        val button: ImageButton = v.findViewById(R.id.addButton)
+        button.setOnClickListener{
+            showdialog(v)
+        }
+
         return v
     }
+
+    fun showdialog(view: View){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Add Vehicle")
+
+        val layout = LinearLayout(requireContext())
+        layout.orientation = LinearLayout.VERTICAL
+
+        val input = EditText(requireContext())
+        input.setHint("Car Name")
+        layout.addView(input)
+
+        val input2 = EditText(requireContext())
+        input2.setHint("License Plate")
+        layout.addView(input2)
+
+        builder.setView(layout)
+
+            // Set up the buttons
+            .setPositiveButton("Add", DialogInterface.OnClickListener() { dialog, which ->
+                // Here you get get input text from the Edittext
+                m_Text = input.text.toString()
+                m_Text2 = input2.text.toString()
+                addMatricula()
+            })
+        builder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+
+        builder.show()
+    }
+
+    //POST Request to Add Vehicle
+    private fun addMatricula(){
+
+        val jsonObject = JSONObject()
+        jsonObject.put("idUtilizador", utilizador.id)
+        jsonObject.put("nomeCarro", m_Text)
+        jsonObject.put("matricula", m_Text2)
+
+        val jsonObjectString = jsonObject.toString()
+
+        val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
+
+        val request = ServiceBuilder.buildService(MatriculaEndpoint::class.java)
+        val call = request.addMatricula(requestBody)
+
+        call.enqueue(object : Callback<Matricula>{
+            override fun onResponse(call: Call<Matricula>, response: Response<Matricula>) {
+                Log.d("TAG", utilizador.id.toString())
+                val c: Matricula = response.body()!!
+                Toast.makeText(requireActivity(), utilizador.id.toString() + "-" + c.nomeCarro, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onFailure(call: Call<Matricula>, t: Throwable) {
+                Toast.makeText(requireActivity(), "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+
+
 }
 //teste
